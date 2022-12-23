a7a3032c45d737a892161447a538d324595794fa
Rafael Malanco <rafael.malanco@softtek.com>
2022-12-23 17:12:59 -0600

    se crea commit

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
--- src/app/unicorns/login/login.module.ts
+++ src/app/unicorns/login/login.module.ts
@@ -4,9 +4,19 @@ import { LoginComponent } from './login.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [LoginRouteModule, FormsModule, MatSnackBarModule],
+  imports: [
+    LoginRouteModule,
+    FormsModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   declarations: [LoginComponent],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
